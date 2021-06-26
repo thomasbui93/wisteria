bc644a94e1f2ab6af6ae9eac10600daddc4f704e
Khoa Bui <kbui@zendesk.com>
2021-06-27 00:27:43 +0100

    Initial starter

---
+++ src/main/java/com/buidangkhoa/wisteria/WisteriaApplication.java
@@ -0,0 +1,13 @@
+package com.buidangkhoa.wisteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buidangkhoa/wisteria/WisteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buidangkhoa.wisteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
